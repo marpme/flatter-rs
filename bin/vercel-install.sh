a892dc10b5fe1aebac0b986f423e5a08f02cf09a
marpme <7430964+marpme@users.noreply.github.com>
2022-11-13 15:35:28 +0100

    fix build with tailwindcss

---
+++ bin/vercel-install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+npm i -g tailwindcss
+
+curl https://sh.rustup.rs -sSf | sh -s -- -y
+PATH=$PATH:/vercel/.cargo/bin
+
+rustup target add wasm32-unknown-unknown
+cargo install wasm-bindgen-cli
+cargo install --locked trunk
